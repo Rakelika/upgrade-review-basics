692dd318be3dc6b4ac9cc0461f62a0f2ad999695
Rakelika <raquelsg.gp@gmail.com>
2022-12-21 21:05:21 +0100

    Iteration#2

---
+++ ejercicio-2.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let favoriteVolume = []
+
+for (const user of users) {
+    for (const key in user.favoritesSounds) {
+        favoriteVolume.push(user.favoritesSounds[key].volume)
+    }   
+}
+
+let mediaVolume = 0;
+
+    for (let i = 0; i < favoriteVolume.length; i++) {
+        mediaVolume += favoriteVolume[i] / favoriteVolume.length;
+    }
+
+
+console.log(mediaVolume)
+
+
